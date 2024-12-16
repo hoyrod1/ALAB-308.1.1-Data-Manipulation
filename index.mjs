a521a827526e5fc77839db06d0e5f4443f6f749c
Rodney St Cloud <hoyrod1@aol.com>
2024-12-15 20:19:39 -0500

    First Assignment to check if the given numbers are divisible by 5

--- index.mjs
+++ index.mjs
@@ -8,23 +8,27 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = n1 + n2 + n3 + n4 == 50;
+console.log(isSum50);
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+console.log(isTwoOdd);
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+console.log(isOver25);
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+console.log(isUnique);
 
 // Here, we put the results into a single variable
 // for convenience. Note how we negate isOver25 using
@@ -49,3 +53,11 @@ const dontDoThis =
   n2 != n3 &&
   n2 != n4 &&
   n3 != n4;
+console.log(dontDoThis);
+//==================================================================================//
+
+//==================================================================================//
+// Check if all numbers are divisible by 5. Cache the result in a variable.
+const isDivisibleBy5 = n1 % 5 === 0 && n2 % 5 === 0 && n3 % 5 === 0 && n4 % 5 === 0;
+console.log(isDivisibleBy5);
+//==================================================================================//
